f93b682821645f060c24f6873e643c3f5bac408d
Vlad Lebedev <vladlebedeff@gmail.com>
2025-03-31 18:31:52 +0300

    Initial implementation (#1)

--- lib/ckmeans/clusterer.rb
+++ lib/ckmeans/clusterer.rb
@@ -1,7 +1,330 @@
 # frozen_string_literal: true
 
 module Ckmeans
-  class Clusterer # rubocop:disable Style/Documentation
-    def initialize(kmin, kmax, entries); end
+  class SmawkClusterer # rubocop:disable Style/Documentation, Metrics/ClassLength
+    attr_reader :xcount, :xsorted, :kmin, :kmax, :smat, :jmat, :kestimate
+
+    PI_DOUBLE = Math::PI * 2
+
+    def initialize(entries, kmin, kmax = kmin, kestimate = :regular)
+      @xcount = entries.size
+
+      raise ArgumentError, "Minimum cluster count is bigger than element count" if kmin > @xcount
+      raise ArgumentError, "Maximum cluster count is bigger than element count" if kmax > @xcount
+
+      @kmin = kmin
+      @unique_xcount = entries.uniq.size
+      @kmax = [@unique_xcount, kmax].min
+      @xsorted = entries.sort
+      @kestimate = kestimate
+    end
+
+    def clusters
+      @clusters ||=
+        if @unique_xcount <= 1
+          [xsorted]
+        else
+          @smat = Array.new(kmax) { Array.new(xcount) { 0.0 } }
+          @jmat = Array.new(kmax) { Array.new(xcount) { 0 } }
+
+          kappa = kmax
+          n = xcount
+          xsum = Array.new(n)
+          xsumsq = Array.new(n)
+          shift = xsorted[n / 2]
+          xsum[0] = xsorted[0] - shift
+          xsumsq[0] = xsum[0]**2
+          1.upto(n - 1) do |i|
+            xsum[i] = xsum[i - 1] + xsorted[i] - shift
+            xsumsq[i] = xsumsq[i - 1] + ((xsorted[i] - shift) * (xsorted[i] - shift))
+            smat[0][i] = dissim(0, i, xsum, xsumsq)
+            jmat[0][i] = 0
+          end
+
+          kappa_dec = kappa - 1
+          1.upto(kappa_dec) do |q|
+            imin = q < kappa_dec ? [1, q].max : n - 1
+            fill_row_q_linear(imin, n - 1, q, smat, jmat, xsum, xsumsq)
+          end
+
+          kopt = select_levels_3_4_12(jmat)
+
+          results = []
+          backtrack(jmat, kopt) do |q, left, right|
+            results[q] = xsorted[left..right]
+          end
+          results
+        end
+    end
+
+    private
+
+    def range_of_variance(x)
+      dposmin = x[-1] - x[0]
+      dposmax = 0.0
+
+      (1...x.size).each do |n|
+        d = x[n] - x[n - 1]
+        dposmin = d if d > 0 && dposmin > d
+        dposmax = d if d > dposmax
+      end
+
+      variance_min = dposmin * dposmin / 3.0
+      variance_max = dposmax * dposmax
+
+      [variance_min, variance_max]
+    end
+
+    def select_levels_3_4_12(jmat)
+      kopt = kmin
+      n = xcount
+      max_bic = 0.0
+      adjustment = kestimate == :sensitive ? 0.0 : 1.0 # deviation from BIC to favor more clusters
+
+      kmin.upto(kmax) do |k|
+        sizes = Array.new(k)
+        backtrack(jmat, k) { |q, left, right| sizes[q] = right - left + 1 }
+        index_left = 0
+        index_right = nil
+        loglikelihood = 0.0
+        bin_left = nil
+        bin_right = nil
+
+        k.times do |kb|
+          num_points_in_bin = sizes[kb]
+          index_right = index_left + num_points_in_bin - 1
+
+          if xsorted[index_left] < xsorted[index_right]
+            bin_left = xsorted[index_left]
+            bin_right = xsorted[index_right]
+          elsif xsorted[index_left] == xsorted[index_right]
+            bin_left = index_left == 0 ? xsorted[0] : (xsorted[index_left - 1] + xsorted[index_left]) / 2.0
+            bin_right = index_right < n - 1 ? (xsorted[index_right] + xsorted[index_right + 1]) / 2.0 : xsorted[n - 1]
+          else
+            raise "ERROR: binLeft > binRight"
+          end
+
+          bin_width = bin_right - bin_left
+
+          mean, variance = shifted_data_variance(index_left, index_right)
+
+          if variance > 0
+            (index_left..index_right).each do |i|
+              loglikelihood += -(xsorted[i] - mean) * (xsorted[i] - mean) / (2.0 * variance)
+            end
+            loglikelihood +=
+              num_points_in_bin *
+              ((Math.log(num_points_in_bin / n.to_f) * adjustment) - (0.5 * Math.log(PI_DOUBLE * variance)))
+          else
+            loglikelihood += num_points_in_bin * Math.log(1.0 / bin_width / n)
+          end
+
+          index_left = index_right + 1
+        end
+
+        bic = (2.0 * loglikelihood) - (((3 * k) - 1) * Math.log(n.to_f))
+
+        if k == kmin
+          max_bic = bic
+          kopt = kmin
+        elsif bic > max_bic
+          max_bic = bic
+          kopt = k
+        end
+      end
+
+      kopt
+    end
+
+    def shifted_data_variance(ileft, iright)
+      sum = 0.0
+      sumsq = 0.0
+      mean = 0.0
+      variance = 0.0
+      n = iright - ileft + 1
+
+      if iright >= ileft
+        median = xsorted[(ileft + iright) / 2]
+
+        ileft.upto(iright) do |i|
+          sumi = xsorted[i] - median
+          sum += sumi
+          sumsq += sumi**2
+        end
+
+        mean = (sum / n) + median
+        variance = (sumsq - (sum * sum / n)) / (n - 1) if n > 1
+      end
+
+      [mean, variance]
+    end
+
+    def backtrack(jmat, k)
+      n = jmat[0].size
+      right = n - 1
+      left = nil
+
+      (k - 1).downto(0) do |q|
+        left = jmat[q][right]
+
+        yield q, left, right
+
+        right = left - 1 if q > 0
+      end
+    end
+
+    def dissim(j, i, xsum, xsumsq)
+      sji = 0.0
+
+      if j >= i
+        sji = 0.0
+      elsif j > 0
+        muji = (xsum[i] - xsum[j - 1]) / (i - j + 1)
+        sji = xsumsq[i] - xsumsq[j - 1] - ((i - j + 1) * muji * muji)
+      else
+        sji = xsumsq[i] - (xsum[i] * xsum[i] / (i + 1))
+      end
+
+      [0, sji].max
+    end
+
+    def fill_row_q_linear(imin, imax, q, smat, jmat, xsum, xsumsq)
+      size = imax - q + 1
+
+      js = Array.new(size) { |i| q + i }
+      smawk(imin, imax, 1, q, js, smat, jmat, xsum, xsumsq)
+    end
+
+    def smawk(imin, imax, istep, q, js, smat, jmat, xsum, xsumsq)
+      if (imax - imin) <= (0 * istep)
+        find_min_from_candidates(imin, imax, istep, q, js, smat, jmat, xsum, xsumsq)
+      else
+        js_odd = js_reduced(imin, imax, istep, q, js, smat, jmat, xsum, xsumsq)
+        istepx2 = istep * 2
+        imin_odd = imin + istep
+        imax_odd = imin_odd + ((imax - imin_odd) / istepx2 * istepx2)
+        smawk(imin_odd, imax_odd, istepx2, q, js_odd, smat, jmat, xsum, xsumsq)
+        fill_even_positions(imin, imax, istep, q, js, smat, jmat, xsum, xsumsq)
+      end
+    end
+
+    def find_min_from_candidates(imin, imax, istep, q, js, smat, jmat, xsum, xsumsq)
+      rmin_prev = 0
+
+      (imin..imax).step(istep) do |i|
+        rmin = rmin_prev
+        smat[q][i] = smat[q - 1][js[rmin] - 1] + dissim(js[rmin], i, xsum, xsumsq)
+        jmat[q][i] = js[rmin]
+
+        ((rmin + 1)...js.size).each do |r|
+          jabs = js[r]
+
+          next if jabs < jmat[q - 1][i]
+          break if jabs > i
+
+          sj = smat[q - 1][jabs - 1] + dissim(jabs, i, xsum, xsumsq)
+
+          next unless sj <= smat[q][i]
+
+          smat[q][i] = sj
+          jmat[q][i] = js[r]
+          rmin_prev = r
+        end
+      end
+    end
+
+    def js_reduced(imin, imax, istep, q, js, smat, _jmat, xsum, xsumsq)
+      n = ((imax - imin) / istep) + 1
+      js_red = js.dup
+
+      return js_red if n >= js.size
+
+      left = -1
+      right = 0
+      m = js_red.size
+
+      while m > n
+        p = left + 1
+        i = imin + (p * istep)
+        j = js_red[right]
+        sl = smat[q - 1][j - 1] + dissim(j, i, xsum, xsumsq)
+        jplus1 = js_red[right + 1]
+        splus1 = smat[q - 1][jplus1 - 1] + dissim(jplus1, i, xsum, xsumsq)
+
+        if (sl < splus1) && (p < n - 1)
+          left += 1
+          js_red[left] = j
+          right += 1
+        elsif (sl < splus1) && (p == n - 1)
+          right += 1
+          js_red[right] = j
+          m -= 1
+        else
+          if p > 0
+            js_red[right] = js_red[left]
+            left -= 1
+          else
+            right += 1
+          end
+
+          m -= 1
+        end
+      end
+
+      ((left + 1)...m).each do |r|
+        js_red[r] = js_red[right]
+        right += 1
+      end
+
+      js_red.slice!(m..-1) if js_red.size > m
+      js_red
+    end
+
+    def fill_even_positions(imin, imax, istep, q, js, smat, jmat, xsum, xsumsq)
+      n = js.size
+      istepx2 = istep * 2
+      jl = js[0]
+
+      i = imin
+      r = 0
+      while i <= imax
+        r += 1 while js[r] < jl
+
+        smat[q][i] = smat[q - 1][js[r] - 1] + dissim(js[r], i, xsum, xsumsq)
+        jmat[q][i] = js[r]
+        jh         = ((i + istep) <= imax ? jmat[q][i + istep] : js[n - 1]).to_i
+        jmax       = [jh, i].min.to_i
+        sjimin     = dissim(jmax, i, xsum, xsumsq)
+
+        r += 1
+        while r < n && js[r] <= jmax
+          jabs = js[r]
+
+          break if jabs > i
+
+          if jabs < jmat[q - 1][i]
+            r += 1
+            next
+          end
+
+          s  = dissim(jabs, i, xsum, xsumsq)
+          sj = smat[q - 1][jabs - 1] + s
+
+          if sj <= smat[q][i]
+            smat[q][i] = sj
+            jmat[q][i] = js[r]
+          elsif smat[q - 1][jabs - 1] + sjimin > smat[q][i]
+            break
+          end
+
+          r += 1
+        end
+
+        r -= 1
+        jl = jh
+
+        i += istepx2
+      end
+    end
   end
 end
--- spec/ckmeans/clusterer_spec.rb
+++ spec/ckmeans/clusterer_spec.rb
@@ -1,9 +1,139 @@
 # frozen_string_literal: true
 
-RSpec.describe Ckmeans::Clusterer do
+RSpec.describe Ckmeans::SmawkClusterer do
   describe "#new" do
     specify do
-      expect { described_class.new(0, 0, []) }.to_not raise_error
+      expect { described_class.new([], 0, 0) }.to_not raise_error
+      expect { described_class.new([], 0) }.to_not raise_error
+    end
+
+    context "when min cluster count > element count" do
+      specify do
+        expect { described_class.new([], 1) }.to(
+          raise_error(ArgumentError, "Minimum cluster count is bigger than element count")
+        )
+      end
+    end
+
+    context "when min cluster count > element count" do
+      specify do
+        expect { described_class.new([], 0, 1) }.to(
+          raise_error(ArgumentError, "Maximum cluster count is bigger than element count")
+        )
+      end
+    end
+  end
+
+  describe "#clusters" do
+    specify do
+      x = [1]
+      kfixed = described_class.new(x, 1)
+      ksensitive = described_class.new(x, 1, 1, :sensitive)
+      expect(kfixed.clusters).to eq([[1]])
+      expect(ksensitive.clusters).to eq([[1]])
+    end
+
+    specify do
+      x = [1, 1, 1, 1]
+      kflexible = described_class.new(x, 1, 4)
+      ksensitive = described_class.new(x, 1, 4, :sensitive)
+      expect(kflexible.clusters).to eq([[1, 1, 1, 1]])
+      expect(ksensitive.clusters).to eq([[1, 1, 1, 1]])
+    end
+
+    context "small set with two unique values" do
+      let(:x) { [100, 100, 100, 99_999] }
+
+      it "are split with explicit kmin" do
+        expect(described_class.new(x, 2).clusters).to eq([[100, 100, 100], [99_999]])
+      end
+
+      it "penalizes extra clusters with regular sensitivity" do
+        expect(described_class.new(x, 1, 4).clusters).to eq([[100, 100, 100, 99_999]])
+      end
+
+      it "penalizes extra clusters with regular sensitivity" do
+        expect(described_class.new(x, 1, 4, :sensitive).clusters).to eq([[100, 100, 100, 99_999]])
+      end
+    end
+
+    specify do
+      expect(described_class.new([1, 1, 1, 100, 100, 100, 1000, 1000], 1,
+                                 8).clusters).to eq([[1, 1, 1, 100, 100, 100], [1000, 1000]])
+    end
+
+    specify do
+      expect(described_class.new([1, 1, 1, 100, 100, 100, 1000, 1000], 1, 3,
+                                 :sensitive).clusters).to eq([[1, 1, 1, 100, 100, 100], [1000, 1000]])
+    end
+
+    specify do
+      expect(described_class.new([3.5, 3.6, 3.7, 3.1, 1.1, 0.9, 0.8, 2.2, 1.9, 2.1], 1,
+                                 5).clusters).to eq([[0.8, 0.9, 1.1], [1.9, 2.1, 2.2], [3.1, 3.5, 3.6, 3.7]])
+    end
+
+    specify do
+      expect(described_class.new([0.9, 1, 1.1, 1.9, 2, 2.1], 1, 6).clusters).to eq([[0.9, 1, 1.1], [1.9, 2, 2.1]])
+    end
+
+    context "relatively close values" do
+      let(:x) { [0.1, 1.1, 1.2, 1.6, 2.2, 2.5, 2.7, 2.8, 3, 3.1, 7.1] }
+
+      specify "regular sensitivity" do
+        expect(described_class.new(x, 1, 8).clusters).to eq([[0.1, 1.1, 1.2, 1.6, 2.2, 2.5, 2.7, 2.8, 3, 3.1], [7.1]])
+      end
+
+      specify "high sensitivity" do
+        expect(described_class.new(x, 1, 8, :sensitive).clusters).to(
+          eq([[0.1, 1.1, 1.2, 1.6], [2.2, 2.5, 2.7, 2.8, 3, 3.1], [7.1]])
+        )
+      end
+    end
+
+    context "cosine sequence" do
+      let(:x) { (-10..10).map { |i| Math.cos(i) } }
+
+      specify "regular sensitivity" do
+        clusters = described_class.new(x, 1, x.size).clusters
+        expect(clusters.size).to eq(2)
+        expect(clusters.first).to all(be_negative)
+        expect(clusters.last).to all(be_positive)
+      end
+
+      specify "high sensitivity" do
+        clusters = described_class.new(x, 1, x.size, :sensitive).clusters
+        expect(clusters.size).to eq(4)
+        expect(clusters.first).to all(be_negative)
+        expect(clusters[1]).to all(be_negative)
+        expect(clusters[2]).to all(be_positive)
+        expect(clusters.last).to all(be_positive)
+      end
+    end
+
+    specify do
+      expect(described_class.new([-1, 2, -1, 2, 4, 5, 6, -1, 2, -1],
+                                 3).clusters).to eq([[-1, -1, -1, -1], [2, 2, 2], [4, 5, 6]])
+    end
+
+    specify do
+      expect(described_class.new([3, 2, -5.4, 0.1], 4).clusters).to eq([[-5.4], [0.1], [2], [3]])
+    end
+
+    specify do
+      expect(described_class.new(Array.new(10) { |i| i + 1 }, 2).clusters).to eq([[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]])
+    end
+
+    specify do
+      expect(described_class.new([-3, 2.2, -6, 7, 9, 11, -6.3, 75, 82.6, 32.3, -9.5, 62.5, 7, 95.2], 8).clusters).to(
+        eq([[-9.5, -6.3, -6], [-3, 2.2], [7, 7, 9, 11], [32.3], [62.5], [75], [82.6], [95.2]])
+      )
+    end
+
+    it "processes 1000 elements into optimimal cluster count within 3s" do
+      entries = Array.new(1000) { rand * 10_000.0 }
+      clusters = nil
+      bm = Benchmark.measure { clusters = described_class.new(entries, 1, 1000, :sensitive).clusters }
+      expect(bm.total).to be < 3
     end
   end
 end
