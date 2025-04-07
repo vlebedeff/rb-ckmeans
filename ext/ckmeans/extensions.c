d8bd78aae08c8adf8391422aa527b3d224bbe58d
Vlad Lebedev <vladlebedeff@gmail.com>
2025-04-07 09:53:03 +0300

    Move implementations underneath extension init

--- ext/ckmeans/extensions.c
+++ ext/ckmeans/extensions.c
@@ -1,11 +1,13 @@
 #include "ruby.h"
 
-VALUE rb_return_nil(VALUE self) {
-    return Qnil;
-}
+VALUE rb_return_nil(VALUE self);
 
 void Init_extensions(void) {
     VALUE ckmeans_module = rb_const_get(rb_cObject, rb_intern("Ckmeans"));
 
     rb_define_singleton_method(ckmeans_module, "c_do_nothing", rb_return_nil, 0);
+}
+
+VALUE rb_return_nil(VALUE self) {
+    return Qnil;
 }
