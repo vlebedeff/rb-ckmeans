fa90353ec191b52b4df9f8f2d48ded258fe6073b
Vlad Lebedev <vladlebedeff@gmail.com>
2025-05-29 09:35:38 +0300

    Integrate GMM-based optimal K count estimation

--- spec/ckmeans/clusterer_spec.rb
+++ spec/ckmeans/clusterer_spec.rb
@@ -27,26 +27,31 @@ RSpec.describe Ckmeans::Clusterer do # rubocop:disable Metrics/BlockLength
   describe "#clusters" do # rubocop:disable Metrics/BlockLength
     specify "one value" do
       x = [1]
-      kfixed = described_class.new(x, 1)
-      ksensitive = described_class.new(x, 1, 1, :sensitive)
-      expect(kfixed.clusters).to eq([[1]])
-      expect(ksensitive.clusters).to eq([[1]])
+      clusters = described_class.new(x, 1).clusters
+      expect(clusters).to eq([[1]])
     end
 
-    specify "two near values" do
-      x = [10, 11]
-      kfixed = described_class.new(x, 1, 2)
-      ksensitive = described_class.new(x, 1, 2, :sensitive)
-      expect(kfixed.clusters).to eq([[10], [11]])
-      expect(ksensitive.clusters).to eq([[10], [11]])
+    describe "two near values" do
+      let(:x) { [10, 11] }
+
+      specify "fast" do
+        clusters = described_class.new(x, 1, 2).clusters
+        expect(clusters).to eq([[10], [11]])
+      end
+
+      specify "stable" do
+        clusters = described_class.new(x, 1, 2, :gmm).clusters
+        expect(clusters).to eq([[10, 11]])
+      end
     end
 
     specify "multiple dupes of one unique value" do
       x = [1, 1, 1, 1]
-      kflexible = described_class.new(x, 1, 4)
-      ksensitive = described_class.new(x, 1, 4, :sensitive)
-      expect(kflexible.clusters).to eq([[1, 1, 1, 1]])
-      expect(ksensitive.clusters).to eq([[1, 1, 1, 1]])
+
+      fast = described_class.new(x, 1, 4).clusters
+      stable = described_class.new(x, 1, 4, :gmm).clusters
+
+      expect(fast).to eq(stable).and eq([x])
     end
 
     context "small set with two unique values" do
@@ -56,19 +61,17 @@ RSpec.describe Ckmeans::Clusterer do # rubocop:disable Metrics/BlockLength
         expect(described_class.new(x, 2).clusters).to eq([[100, 100, 100], [99_999]])
       end
 
-      it "penalizes extra clusters with regular sensitivity" do
-        expect(described_class.new(x, 1, 4).clusters).to eq([[100, 100, 100, 99_999]])
-      end
+      it "penalizes extra clusters" do
+        fast = described_class.new(x, 1, 4).clusters
+        stable = described_class.new(x, 1, 4, :gmm).clusters
 
-      it "penalizes extra clusters with high sensitivity" do
-        expect(described_class.new(x, 1, 4, :sensitive).clusters).to eq([[100, 100, 100, 99_999]])
+        expect(fast).to eq(stable).and eq([x])
       end
 
       it "splits sequences with more data points" do
-        expect(described_class.new(x + [100_000], 1, 4).clusters).to eq([[100, 100, 100], [99_999, 100_000]])
-        expect(described_class.new(x + [100_000], 1, 4, :sensitive).clusters).to(
-          eq([[100, 100, 100], [99_999, 100_000]])
-        )
+        expect(
+          described_class.new(x + [100_000], 1, 4).clusters
+        ).to eq([[100, 100, 100], [99_999, 100_000]])
       end
     end
 
@@ -96,82 +99,110 @@ RSpec.describe Ckmeans::Clusterer do # rubocop:disable Metrics/BlockLength
           eq([x[0..2], x[3..5], x[6..8], x[9..11], x[12..13], x[14..15]])
         )
       end
+    end
 
-      specify do
-        expect(described_class.new(x, 1, 15, :sensitive).clusters).to eq([x[0..3], x[4..7], x[8..11], x[12..15]])
-      end
+    example "large gaps and dupes" do
+      x = [1, 1, 1, 100, 100, 100, 1000, 1000]
+      kmin = 1
+      kmax = 8
+      fast = described_class.new(x, kmin, kmax).clusters
+      stable = described_class.new(x, kmin, kmax, :gmm).clusters
 
-      specify do
-        expect(described_class.new(x.shuffle, 1, 15, :sensitive).clusters).to(
-          eq([x[0..3], x[4..7], x[8..11], x[12..15]])
-        )
-      end
+      expect(fast).to eq([[1, 1, 1, 100, 100, 100], [1000, 1000]])
+      expect(stable).to eq([[1, 1, 1], [100, 100, 100], [1000, 1000]])
+    end
+
+    example "large gaps distinct" do
+      x = [1, 2, 3, 100, 101, 102, 1000, 1001]
+
+      fast = described_class.new(x, 1, x.size).clusters
+      stable = described_class.new(x, 1, x.size, :gmm).clusters
+
+      expect(fast).to eq(stable).and eq(
+        [[1, 2, 3], [100, 101, 102], [1000, 1001]]
+      )
     end
 
     example do
-      x = [1, 1, 1, 100, 100, 100, 1000, 1000]
-      kmin = 1
-      kmax = 8
-      clusters = described_class.new(x, kmin, kmax).clusters
+      x = [3.5, 3.6, 3.7, 3.1, 1.1, 0.9, 0.8, 2.2, 1.9, 2.1]
 
-      expect(clusters).to eq([[1, 1, 1, 100, 100, 100], [1000, 1000]])
+      fast = described_class.new(x, 1, x.size).clusters
+      stable = described_class.new(x, 1, x.size, :gmm).clusters
+
+      expect(fast).to eq(stable).and eq(
+        [[0.8, 0.9, 1.1], [1.9, 2.1, 2.2], [3.1, 3.5, 3.6, 3.7]]
+      )
     end
 
     specify do
-      x = [1, 1, 1, 100, 100, 100, 1000, 1000]
-      kmin = 1
-      kmax = 8
-      clusters = described_class.new(x, kmin, kmax, :sensitive).clusters
-
-      expect(clusters).to eq([[1, 1, 1, 100, 100, 100], [1000, 1000]])
-    end
-
-    specify do
-      expect(described_class.new([3.5, 3.6, 3.7, 3.1, 1.1, 0.9, 0.8, 2.2, 1.9, 2.1], 1,
-                                 5).clusters).to eq([[0.8, 0.9, 1.1], [1.9, 2.1, 2.2], [3.1, 3.5, 3.6, 3.7]])
-    end
-
-    specify do
-      expect(described_class.new([0.9, 1, 1.1, 1.9, 2, 2.1], 1, 6).clusters).to eq([[0.9, 1, 1.1], [1.9, 2, 2.1]])
+      expect(
+        described_class.new([0.9, 1, 1.1, 1.9, 2, 2.1], 1, 6).clusters
+      ).to eq([[0.9, 1, 1.1], [1.9, 2, 2.1]])
     end
 
     context "relatively close values" do
       let(:x) { [0.1, 1.1, 1.2, 1.6, 2.2, 2.5, 2.7, 2.8, 3, 3.1, 7.1] }
 
-      specify "regular sensitivity" do
-        expect(described_class.new(x, 1, 8).clusters).to eq([[0.1, 1.1, 1.2, 1.6, 2.2, 2.5, 2.7, 2.8, 3, 3.1], [7.1]])
+      example "fast" do
+        expect(described_class.new(x, 1, x.size).clusters).to eq([x[0..-2], [7.1]])
       end
 
-      specify "high sensitivity" do
-        expect(described_class.new(x, 1, 8, :sensitive).clusters).to(
-          eq([[0.1, 1.1, 1.2, 1.6], [2.2, 2.5, 2.7, 2.8, 3, 3.1], [7.1]])
+      example "stable" do
+        expect(described_class.new(x, 1, x.size, :gmm).clusters).to eq([x])
+      end
+    end
+
+    context "bimodal distributions" do
+      let(:x) { [1, 1.1, 1.2, 2, 2.1, 2.2, 15, 15.1, 15.2] }
+      let(:fast) { described_class.new(x, 1, x.size).clusters }
+      let(:stable) { described_class.new(x, 1, x.size, :gmm).clusters }
+
+      specify do
+        expect(fast).to eq(stable).and eq(
+          [[1, 1.1, 1.2], [2, 2.1, 2.2], [15, 15.1, 15.2]]
         )
+      end
+    end
+
+    context "varying density" do
+      let(:x) { [0.1, 0.2, 0.3, 1.0, 2.0, 3.0, 10.0, 10.1, 10.2, 10.3, 10.4, 10.5] }
+      let(:fast) { described_class.new(x, 1, x.size).clusters }
+      let(:stable) { described_class.new(x, 1, x.size, :gmm).clusters }
+
+      specify do
+        expect(fast).to eq(stable).and eq(
+          [[0.1, 0.2, 0.3, 1.0, 2.0, 3.0], [10.0, 10.1, 10.2, 10.3, 10.4, 10.5]]
+        )
+      end
+    end
+
+    context "yet another set" do
+      let(:x) { [1, 1.1, 1.2, 5, 10, 20, 30, 40, 50] }
+      let(:fast) { described_class.new(x, 1, x.size).clusters }
+      let(:stable) { described_class.new(x, 1, x.size, :gmm).clusters }
+
+      specify do
+        expect(fast).to eq(stable).and eq([x])
       end
     end
 
     context "cosine sequence" do
       let(:x) { (-10..10).map { |i| Math.cos(i) } }
+      let(:fast) { described_class.new(x, 1, x.size).clusters }
+      let(:stable) { described_class.new(x, 1, x.size, :gmm).clusters }
 
-      specify "regular sensitivity" do
-        clusters = described_class.new(x, 1, x.size).clusters
-        expect(clusters.size).to eq(2)
-        expect(clusters.first).to all(be_negative)
-        expect(clusters.last).to all(be_positive)
-      end
-
-      specify "high sensitivity" do
-        clusters = described_class.new(x, 1, x.size, :sensitive).clusters
-        expect(clusters.size).to eq(4)
-        expect(clusters.first).to all(be_negative)
-        expect(clusters[1]).to all(be_negative)
-        expect(clusters[2]).to all(be_positive)
-        expect(clusters.last).to all(be_positive)
+      specify do
+        expect(fast).to eq(stable)
+        expect(fast.size).to eq(2)
+        expect(fast.first).to all(be_negative)
+        expect(fast.last).to all(be_positive)
       end
     end
 
     specify do
-      expect(described_class.new([-1, 2, -1, 2, 4, 5, 6, -1, 2, -1],
-                                 3).clusters).to eq([[-1, -1, -1, -1], [2, 2, 2], [4, 5, 6]])
+      expect(
+        described_class.new([-1, 2, -1, 2, 4, 5, 6, -1, 2, -1], 3).clusters
+      ).to eq([[-1, -1, -1, -1], [2, 2, 2], [4, 5, 6]])
     end
 
     specify do
@@ -188,12 +219,27 @@ RSpec.describe Ckmeans::Clusterer do # rubocop:disable Metrics/BlockLength
       )
     end
 
-    it "processes 5k elements into optimimal cluster count within 3s" do
-      n = 5_000
-      x = Array.new(n) { rand * 1_000_000 }
-      bm = Benchmark.measure { described_class.new(x, 1, x.size).clusters }
-      # puts bm.total
-      expect(bm.total).to be < 3
+    describe "performance" do
+      # rubocop:disable Lint/ConstantDefinitionInBlock -- in order to use the values in example descriptions
+      N_FAST = 5_000
+      N_STABLE = N_FAST / 7
+      # rubocop:enable Lint/ConstantDefinitionInBlock
+
+      let(:x) { Array.new(N_FAST) { rand * 1_000_000 } }
+      let(:fast) { described_class.new(x, 1, x.size).clusters }
+      let(:stable) { described_class.new(x.first(N_STABLE), 1, N_STABLE, :gmm).clusters }
+
+      specify "`:fast` mode handles #{N_FAST} elements and K estimation within 3s" do
+        bm = Benchmark.measure { fast }
+        # puts bm.total
+        expect(bm.total).to be < 3
+      end
+
+      specify "`:stable` mode handles #{N_STABLE} elements and K estimation within 3s" do
+        bm = Benchmark.measure { stable }
+        # puts bm.total
+        expect(bm.total).to be < 3
+      end
     end
   end
 end
